c31861707043eb57bde12a7e698f3cda8d5b36df
MIN <nakbkqwe21@gmail.com>
2026-03-26 13:02:22 +0900

    no message

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -13,13 +13,21 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
+<<<<<<< HEAD
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~~~"));
+=======
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+>>>>>>> c906c9e ([Other] MyActor 클래스 리팩토링)
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+<<<<<<< HEAD
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking~~~"));
+=======
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+>>>>>>> c906c9e ([Other] MyActor 클래스 리팩토링)
 }
 
