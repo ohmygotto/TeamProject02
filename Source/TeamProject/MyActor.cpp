537876ab1eca7ab0c9afdab246759d8481a547d3
MIN <nakbkqwe21@gmail.com>
2026-03-26 13:08:24 +0900

    no message

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -13,37 +13,33 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~~~"));
-=======
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
->>>>>>> c906c9e ([Other] MyActor 클래스 리팩토링)
-=======
 
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~~~"));
 
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
 
->>>>>>> 7132353033317351a665f1830783b2fce7adac88
+
+
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~~~"));
+
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking~~~"));
-=======
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
->>>>>>> c906c9e ([Other] MyActor 클래스 리팩토링)
-=======
 
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking~~~"));
 
 	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
 
->>>>>>> 7132353033317351a665f1830783b2fce7adac88
+
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking~~~"));
+
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+
+
 }
 
